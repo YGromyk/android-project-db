d94da58017a1706bbc440c91c490709742d9654a
Yurii Gromyk <yurii.gromyk@magnise.com>
2018-11-17 20:37:44 +0200

    Added splash fragment.

---
+++ app/src/main/java/com/gromyk/roomcodelabs/ui/SplashFragment.kt
@@ -0,0 +1,37 @@
+package com.gromyk.roomcodelabs.ui
+
+import android.os.Bundle
+import android.os.Handler
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.gromyk.roomcodelabs.R
+import com.gromyk.roomcodelabs.ui.base.BaseFragment
+
+/**
+ * Created by Yuriy Gromyk on 11/17/18.
+ */
+
+class SplashFragment : BaseFragment() {
+    companion object {
+        fun newInstance() = SplashFragment()
+    }
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.splash_fragment, container, false)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        startLoginScreen()
+    }
+
+    private fun startLoginScreen() {
+        Handler().postDelayed(
+            { findNavController().navigate(R.id.action_splashFragment_to_authFragment) },
+            2000
+        )
+    }
+}
