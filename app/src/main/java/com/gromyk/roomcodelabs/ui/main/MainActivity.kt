d939d869a72787c6e72b2c21c58bd8a6fe8e6b46
Yurii Gromyk <yurii.gromyk@magnise.com>
2018-11-18 04:02:23 +0200

    Added arguments.

--- app/src/main/java/com/gromyk/roomcodelabs/ui/main/MainActivity.kt
+++ app/src/main/java/com/gromyk/roomcodelabs/ui/main/MainActivity.kt
@@ -2,11 +2,10 @@ package com.gromyk.roomcodelabs.ui.main
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import com.gromyk.api.auth.AppCredentials.redirectUri
 import com.gromyk.roomcodelabs.R
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,17 +23,15 @@ class MainActivity : AppCompatActivity() {
 
     override fun onSupportNavigateUp() = findNavController(R.id.nav_host_fragment).navigateUp()
 
-    override fun onResume() {
-        super.onResume()
+    override fun onPostResume() {
+        super.onPostResume()
         val data = intent.data
-        if (AuthData.uri != null && AuthData.uri.toString().startsWith(redirectUri)) {
-            // use the parameter your API exposes for the code (mostly it's "code")
-            val code = AuthData.uri.getQueryParameter("code")
-            if (code != null) {
-                Log.d(this::class.java.simpleName, code)
-            } else if (AuthData.uri.getQueryParameter("error") != null) {
-                // show an error message here
-            }
+        data?.let {
+            val code = data.getQueryParameter("code")
+            Timber.e("Code id $code")
+            val bundle = Bundle()
+            bundle.putString(getString(R.string.oAuthCode), code)
+            findNavController(R.id.nav_host_fragment).navigate(R.id.action_authFragment_to_signInFragment, bundle)
         }
     }
 }
